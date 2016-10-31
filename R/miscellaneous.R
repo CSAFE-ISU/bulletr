d13de6c1773b8f9a8781098bdd74909c67b50fdf
Eric Hare <ericrhare@gmail.com>
2016-10-31 13:30:55 -0500

    Initial commit

---
+++ R/miscellaneous.R
@@ -0,0 +1,126 @@
+#' Estimate the twist in a bullet land
+#' 
+#' Estimation of the twist in a barrel follows roughly the process described by Chu et al (2010).
+#' At the moment, twist is estimated from a single land - but the twist should be the same for the whole barrel. Therefore all lands of the same barrel should
+#' have the same twist.
+#' A note on timing: at the moment calculating the twist rate for a bullet land takes several minutes.
+#' XXX TODO XXX make the different methods a parameter. Also, accept other input than the path - if we start with the flattened bulletland we get results much faster.
+#' @param path to a file in x3p format
+#' @param bullet data in x3p format as returned by function read.x3p
+#' @param transpose If TRUE, transpose the matrix
+#' @param twistlimit Constraint the possible twist value
+#' @param cutoff Use this for the quantile cutoff
+#' @return numeric value estimating the twist
+#' @export
+#' @importFrom robustbase lmrob
+#' @importFrom stats lm coef median 
+#' @importFrom utils head tail
+#' @examples
+#' \dontrun{
+#' # execution takes several minutes
+#' load("data/b1.rda")
+#' twist <- getTwist(path="barrel 1 bullet 1", bullet = b1, twistlimit=c(-2,0)*1.5625)
+#' }
+getTwist <- function(path, bullet = NULL, transpose = FALSE, twistlimit = NULL, cutoff = .75) {
+    x <- NULL
+    r.squared <- NULL
+    r.squared.robust <- NULL
+    
+  if (is.null(bullet)) bullet <- read.x3pplus(path, transpose = transpose)
+  cat(path)
+  cat("\n")
+  
+  gg115 <- processBullets(
+    bullet, 
+    x=bullet$header.info$x.inc*c(0,bullet$header.info$num.pts.line), 
+    name=path)
+  # qplot(data=gg115, x=y, y=x, fill=resid, geom="tile")+scale_fill_gradient2()
+  
+  xs <- unique(gg115$x)
+  twist <- NULL
+  ccf <- NULL
+  aligned <- list()
+  
+  for (i in seq_along(xs)[-1]) {
+    profiles <- subset(gg115, x %in% xs[c(i,i-1)])  
+    profiles$bullet <- sprintf("%s-%s", path, profiles$x)
+    aligned[[i]] <- bulletAlign(profiles, value="resid")
+    twist <- c(twist, aligned[[i]]$lag)
+    ccf <- c(ccf, aligned[[i]]$ccf)
+  }
+  #qplot(xs, c(0, twist)) + ylim(c(-10,10))
+  #qplot(xs, c(1, ccf)) + geom_hline(yintercept=0.95, colour="red")
+  #qplot(xs, cumsum(c(0, twist))) +ylim(c(700,1100))
+
+#  if (!is.null(twistlimit)) {
+    # is twistlimit a vector with two numbers?
+    twistConstraint <- pmin(twist, max(twistlimit))
+    twistConstraint <- pmax(twistConstraint, min(twistlimit))
+#  } else twistConstraint = twist
+  dframe <- data.frame(x = xs, twist=cumsum(c(0, twist)), 
+                       twistConstraint = cumsum(c(0, twistConstraint)), 
+                       ccf = c(0,ccf))
+  #qplot(x, twist, data=subset(dframe, between(x, 220, 600))) +geom_smooth(method="lm")
+  
+  Rs <- data.frame(zoo::rollapply(data=dframe$twist, width=200, FUN=function(twist) {
+    x <- 1:length(twist)
+    m <- lm(twist~x)
+    m2 <- try(robustbase::lmrob(twist~x, na.action=na.omit), silent=TRUE)
+    if (class(m2) == "try-error") {
+      cat(sprintf("NAs in robust estimation of twist in land %s\n", path))
+ #     browser()
+      r.squared.robust=NA
+      twistRobust=NA
+    } else {
+      r.squared.robust=summary(m2)$r.squared
+      twistRobust=coef(m2)[2]
+    }
+    
+    data.frame(r.squared=summary(m)$r.squared, twist=coef(m)[2],
+               r.squared.robust=r.squared.robust, 
+               twistRobust=twistRobust)
+  }, by=1))
+#  browser()
+  RConstraint <- data.frame(zoo::rollapply(data=dframe$twistConstraint, width=200, FUN=function(twist) {
+    x <- 1:length(twist)
+    m <- lm(twist~x)
+    m2 <- try(robustbase::lmrob(twist~x, na.action=na.omit), silent=TRUE)
+    if (class(m2) == "try-error") {
+      cat(sprintf("NAs in robust estimation of twist in land %s\n", path))
+ #     browser()
+      r.squared.robust=NA
+      twistRobust=NA
+    } else {
+      r.squared.robust=summary(m2)$r.squared
+      twistRobust=coef(m2)[2]
+    }
+    data.frame(r.squared=summary(m)$r.squared, twist=coef(m)[2],
+               r.squared.robust=r.squared.robust, 
+               twistRobust=twistRobust)
+  }, by=1))
+  
+    
+  q75 <- quantile(Rs$r.squared, probs=cutoff)
+  twist <- median(subset(Rs, r.squared > q75)$twist)
+  q75r <- quantile(Rs$r.squared.robust, probs=cutoff, na.rm=TRUE)
+  twistRobust <- median(subset(Rs, r.squared.robust > q75r)$twistRobust, na.rm=TRUE)
+  
+  q75C <- quantile(RConstraint$r.squared, probs=cutoff)
+  twistC <- median(subset(RConstraint, r.squared > q75)$twist)
+  q75rC <- quantile(RConstraint$r.squared.robust, probs=cutoff, na.rm=TRUE)
+  twistRobustC <- median(subset(RConstraint, r.squared.robust > q75r)$twistRobust, na.rm=TRUE)
+  
+  
+    
+  data.frame(twist=twist, min.r.squared=q75, twistRobust=twistRobust, min.r.squared.robust=q75r,
+             twistC=twistC, min.r.squaredC=q75C, twistRobustC=twistRobustC, min.r.squared.robustC=q75rC)
+}
+
+#' @importFrom plotly plot_ly
+plot_3d_land <- function(path, bullet, groove, x = 99.84) {
+    br111 <- read.x3p(path)
+    inds <- which(bullet$y > groove$groove[1] & bullet$y < groove$groove[2])
+    surfmat <- br111$surface.matrix
+    
+    plot_ly(z = surfmat[inds,], type = "surface")
+}
---
+++ R/x3p.R
@@ -0,0 +1,110 @@
+#' Read an x3p file as an R Data Frame
+#' 
+#' @param path The file path to the x3p file
+#' @param transpose If TRUE, transpose the resulting matrix
+#' 
+#' @export
+#' @importFrom xml2 
+read_x3p <- function(path, transpose = FALSE) {
+    bullet_info <- read_xml(result[2]) 
+
+    if (transpose) {
+        bullet$surface.matrix <- t(bullet$surface.matrix)
+        temp <- bullet$header.info$num.pts.line
+        bullet$header.info$num.pts.line <- bullet$header.info$num.lines
+        bullet$header.info$num.lines <- temp
+    } 
+    
+    return(bullet)
+}
+
+#' Convert a list of x3p file into a data frame
+#' 
+#' x3p format consists of a list with header info and a 2d matrix of scan depths. 
+#' fortify_x3p turn the matrix into a variable within a data frame, using the parameters of the header as necessary.
+#' @param x3p a file in x3p format as return by function read.x3p
+#' @return data frame with variables x, y, and value
+#' @export
+fortify_x3p <- function(x3p) {
+    info <- x3p[[1]]
+    
+    df <- data.frame(expand.grid(x=1:info$num.pts.line, y=1:info$num.lines), 
+                     value=as.vector(t(x3p[[2]])))
+    df$x <- (df$x-1) * info$x.inc
+    df$y <- (df$y-1) * info$y.inc
+    
+    attr(df, "info") <- info
+    
+    df
+}
+
+#' Convert a data frame into an x3p file
+#' 
+#' @param df A data frame produced by fortify_x3p
+#' @return An x3p object
+#' @export
+unfortify_x3p <- function(df) {
+    my.info <- attr(df, "info")
+    my.lst <- list(header.info = my.info, 
+                   surface.matrix = matrix(df$value, 
+                                           nrow = length(unique(df$y)), 
+                                           ncol = length(unique(df$x)),
+                                           byrow = TRUE))
+    
+    return(my.lst)
+}
+
+#' Sample every X element of a data frame
+#' 
+#' Sample every X element of a data frame in x and y direction
+#' @param dframe data frame with x and y variable
+#' @param byxy (vector) of numeric value indicating the sapling resolution. If a single number, the same resolution is used for x and y.
+#' @return subset of the input variable
+#' @export
+sample.x3p <- function(dframe, byxy=c(2,2)) {
+    x <- NULL
+    y <- NULL
+    # use fortified data set
+    # use only every byxy sample in x and y direction 
+    if(length(byxy)==1) byxy <- rep(byxy, length=2)
+    xn <- sort(as.numeric(unique(dframe$x)))
+    yn <- sort(as.numeric(unique(dframe$y)))
+    
+    xseq <- xn[seq(1, length(xn), by=byxy[1])] 
+    yseq <- yn[seq(1, length(yn), by=byxy[2])] 
+    subset(dframe, (x %in% xseq) & (y %in% yseq))
+}
+
+#' Process x3p file 
+#' 
+#' x3p file of a 3d topological bullet surface is processed at surface crosscut x, 
+#' the bullet grooves in the crosscuts are identified and removed, and a loess smooth 
+#' is used (see \code{?loess} for details) to remove the big structure. 
+#' @param bullet file as returned from read.x3p
+#' @param name name of the bullet
+#' @param x (vector) of surface crosscuts to process. 
+#' @param grooves The grooves to use as a two element vector, if desired
+#' @param span The span for the loess fit
+#' @param ... Additional arguments, passed to the get_grooves function
+#' @return data frame
+#' @importFrom dplyr bind_rows %>%
+#' @export
+processBullets <- function(bullet, name = "", x = 100, grooves = NULL, span = 0.75, ...) {
+    crosscuts <- unique(fortify_x3p(bullet)$x)
+    crosscuts <- crosscuts[crosscuts >= min(x)]
+    crosscuts <- crosscuts[crosscuts <= max(x)]
+    if (length(x) > 2) crosscuts <- crosscuts[crosscuts %in% x]
+    
+    list_of_fits <- lapply(crosscuts, function(x) {
+        br111 <- get_crosscut(path = NULL, x = x, bullet = bullet)
+        if (is.null(grooves)) {
+            br111.groove <- get_grooves(br111, ...)
+        } else {
+            br111.groove <- list(groove = grooves)
+        }
+        fit_loess(br111, br111.groove, span = span)$resid$data
+    })
+    lof <- list_of_fits %>% bind_rows
+    
+    data.frame(lof, bullet = name, stringsAsFactors = FALSE)
+}

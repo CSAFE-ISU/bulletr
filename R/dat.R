3df2b2b03f1ce57fc9fc32c2e9b9743f924f3b92
Heike Hofmann <hofmann@iastate.edu>
2017-05-02 21:50:15 -0500

    read_dat command added

---
+++ R/dat.R
@@ -0,0 +1,40 @@
+#' Read a dat file and create an x3p file
+#' 
+#' @param path The file path to the dat file
+#' @format list with header information and surface matrix
+#' @export
+#' @importFrom readr read_delim
+#' @examples
+#' \dontrun{
+#' d1 <- read_dat("Br4 Bullet 4-1.dat")
+#' }
+#'
+read_dat <- function(path) {
+  g1 <- read_delim(path, 
+                       delim = " ", 
+                       col_names = c("y", "x", "value"))
+  
+  g1_clean <- g1 %>% 
+    dplyr::select(x, y, value) %>% 
+    arrange(y, x) %>%
+    mutate(value = as.numeric(ifelse(value == "1.#QNAN0" | value == "-1.#IND00", NaN, value))) %>%
+    mutate(value = value - min(value, na.rm = TRUE))
+  
+  g1_inc_x <- diff(unique(g1_clean$x)[1:2])
+  g1_inc_y <- diff(unique(g1_clean$y)[1:2])
+  
+  g1_num_profiles <- length(unique(g1_clean$x))
+  g1_num_obs_per_profile <- length(unique(g1_clean$y))
+  
+  g1_header.info <- list(num_profiles = g1_num_profiles,
+                         num_obs_per_profile = g1_num_obs_per_profile,
+                         profile_inc = g1_inc_x,
+                         obs_inc = g1_inc_y)
+  
+  g1_mat <- matrix(g1_clean$value, 
+                   nrow = g1_num_obs_per_profile, 
+                   ncol = g1_num_profiles, byrow = TRUE)
+  
+  list(header.info = g1_header.info, surface.matrix = g1_mat)
+}
+

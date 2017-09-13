3d15fdefabb449fe5116262ea30e18203ff5ec66
Heike Hofmann <hofmann@iastate.edu>
2017-09-13 16:46:38 -0500

    faster method for image

--- R/image.R
+++ R/image.R
@@ -11,7 +11,37 @@
 #' data(br411)
 #' image(br411, file = "br411.png")
 image.x3p <- function(x, file = NULL, col = "#cd7f32", ...) {
-#  browser()
+  #  browser()
+  surface <- x$surface.matrix
+  z <- 2*surface # Exaggerate the relief
+  y <- x$header.info$profile_inc * (1:ncol(z)) # 
+  x <- x$header.info$obs_inc * (1:nrow(z)) # 
+  
+  params <- rgl::r3dDefaults
+#  params$viewport <- c(0,0, 750, 250)
+#  
+  params$windowRect <- c(40, 125, 790, 375)
+  params$userMatrix <- diag(c(1,1,1,1))
+  params$zoom <- 0.35
+  
+  open3d(params=params)
+  rgl.pop("lights")
+  xyz <- matrix(c(-2000, mean(y), max(z, na.rm=TRUE)), ncol = 3)
+  light3d(x = xyz, diffuse = "gray40", 
+          specular = "gray40", ambient="grey10", viewpoint.rel = TRUE) 
+  light3d(diffuse = "gray20", specular = "gray20")
+  
+  surface3d(x, y, z, color = col, back = "lines")
+
+  if (!is.null(file)) {
+    rgl.snapshot(file=file)
+    rgl.close()
+  }
+}
+
+# old implementation
+image.x3p.old <- function(x, file = NULL, col = "#cd7f32", ...) {
+  #  browser()
   surface <- x$surface.matrix
   z <- 2*surface # Exaggerate the relief
   y <- x$header.info$profile_inc * (1:ncol(z)) # 
@@ -20,11 +50,11 @@ image.x3p <- function(x, file = NULL, col = "#cd7f32", ...) {
   zlim <- range(z, na.rm=TRUE)
   zlen <- zlim[2] - zlim[1] + 1
   #col <- colorlut[ z - zlim[1] + 1 ] # assign colors to heights for each point
-#  col <- "#cd7f32" # bronze
-#  col <- "darkred"
-#  col <- "#C0C0C0" #silver
+  #  col <- "#cd7f32" # bronze
+  #  col <- "darkred"
+  #  col <- "#C0C0C0" #silver
   
-#  col <- "#b87333" #copper
+  #  col <- "#b87333" #copper
   
   
   params <- rgl::r3dDefaults
@@ -38,13 +68,13 @@ image.x3p <- function(x, file = NULL, col = "#cd7f32", ...) {
   light3d(x = xyz, diffuse = "gray40", 
           specular = "gray40", ambient="grey10", viewpoint.rel = TRUE) 
   light3d(diffuse = "gray20", specular = "gray20")
-
+  
   save <- par3d()
   save$viewport[3:4] <- c(750, 250)
   save$windowRect <- c(40, 125, 790, 375)
-suppressWarnings({
+  suppressWarnings({
     par3d(save)
-})
+  })
   save$zoom <- 0.35
   #bg3d("white")
   suppressWarnings({
@@ -53,7 +83,7 @@ suppressWarnings({
   
   Sys.sleep(2)
   if (!is.null(file)) {
-  rgl.snapshot(file=file)
-  rgl.close()
+    rgl.snapshot(file=file)
+    rgl.close()
   }
 }

aaf40e289a97ec0107ea59e1a4c8116041240d7a
Heike Hofmann <hofmann@iastate.edu>
2018-02-02 10:42:21 -0600

    additional error checking in read_x3p

--- R/x3p.R
+++ R/x3p.R
@@ -14,16 +14,22 @@
 #' }
 #'
 read_x3p <- function(path, profiley = TRUE, automatic = TRUE) {
+
     ## Create a temp directory to unzip x3p file
     mydir <- tempdir()
     result <- unzip(path, exdir = mydir)
+    ## see what we got: 
+    data <- grep(".bin$", result) # data has extension .bin
+    meta <- grep(".xml$", result) # meta info has extension .xml
+    # if we have not exactly one of each we have a problem:
+    stopifnot(length(data)==1, length(meta) == 1) # nice error messages would be good
     
     ## Should contain data.bin and valid.bin
     bullet_data_dir <- file.path(mydir, "bindata", dir(file.path(mydir, "bindata")))
-    bullet_data <- result[1]
+    bullet_data <- result[data]
     
     ## Get the information on the bullet
-    bullet_info <- read_xml(result[2])
+    bullet_info <- read_xml(result[meta])
     bullet_children <- xml_children(bullet_info)
     bullet_childinfo <- xml_children(bullet_children)
     
